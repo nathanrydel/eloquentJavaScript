7d08ff61bd088b2dd5ff9a9cc32f1b53674c9e09
nathanrydel <nathanrydel@gmail.com>
2023-05-08 20:34:47 -0700

    Challenge FizzBuzz === complete

--- Chapter 2/FizzBuzz.js
+++ Chapter 2/FizzBuzz.js
@@ -5,3 +5,11 @@ Write a program that uses console.log to print all the numbers from 1 to 100, wi
 When you have that working, modify your program to print "FizzBuzz" for numbers that are divisible by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
 
 */
+
+for (let i = 1; i <= 15; i++) {
+  let output = '';
+  if (i % 3 === 0) output += 'Fizz';
+  if (i % 5 === 0) output += 'Buzz';
+  console.log(output || i);
+}
+
