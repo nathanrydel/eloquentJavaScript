a8369f3b0ac274011222bb4f4bc7a2de5a99560c
nathanrydel <nathanrydel@gmail.com>
2023-05-08 21:40:07 -0700

    Challenge Minimum === complete

--- Chapter 3/minimum.js
+++ Chapter 3/minimum.js
@@ -1,12 +1,19 @@
 /* Minimum
 The previous chapter introduced the standard function Math.min that returns its smallest argument. We can build something like that now. Write a function min that takes two arguments and returns their minimum.
+*/
 
 // Your code here.
+function min(x, y) {
+  if (x < y) return x;
+  else return y;
+}
+
 
 console.log(min(0, 10));
 // → 0
 console.log(min(0, -10));
 // → -10
+/*
 If you have trouble putting braces and parentheses in the right place to get a valid function definition, start by copying one of the examples in this chapter and modifying it.
 
 A function may contain multiple return statements.
